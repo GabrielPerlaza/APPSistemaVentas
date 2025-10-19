3b768d2f7a566a71a1728842fca9a34c4b342039
unknown <gabrieldeberes@gmail.com>
2025-10-19 00:33:23 -0500

    Arreglando problema con endpoint

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
+    production: true,
     endpoint: "http://localhost:5031/api/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
+    production: false,
     endpoint: "https://apisistemaventas-latest.onrender.com/api/",
 };
